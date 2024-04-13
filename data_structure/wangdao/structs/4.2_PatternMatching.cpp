1fc37a5511cdcb8738f45470ada392c0b0bbe2e0
skyholeBishop <51252247+bigLRRH@users.noreply.github.com>
2024-04-13 20:44:27 +0800

    4/12

--- data_structure/wangdao/structs/4.2_PatternMatching.cpp
+++ data_structure/wangdao/structs/4.2_PatternMatching.cpp
@@ -1,5 +1,5 @@
 #define MAXSTRLEN 255
-typedef char SString[MAXSTRLEN + 1];
+typedef char SString[MAXSTRLEN + 1]; // SString[0] is length
 
 // 4.2.1 brute force
 // Str = string
@@ -25,5 +25,49 @@ int index(SString mainStr, SString subStr)
         return i - subStr[0];
     return 0;
 }
+// n = mainStr.length()
+// m = subStr.length()
+// tn=O(m*n)
 
 // 4.2.2 kmp
+void get_next(SString subStr, int next[])
+{
+    int i = 1, j = 0;
+    next[1] = 0;
+    while (i < subStr[0])
+    {
+        if (j == 0 || subStr[i] == subStr[j])
+        {
+            ++i;
+            ++j;
+            next[i] = j;
+        }
+        else
+            j = next[j];
+    }
+}
+// m = subStr.length()
+// tn=o(m)
+
+// 4.2.3 better kmp
+void get_nextVal(SString subStr, int nextVal[])
+{
+    int i = 1, j = 0;
+    nextVal[1] = 0;
+    while (i < subStr[0])
+    {
+        if (j == 0 || subStr[i] == subStr[j])
+        {
+            ++i;
+            ++j;
+            if (subStr[i] != subStr[j])
+            {
+                nextVal[i] = j;
+            }
+            else
+                nextVal[i] = nextVal[j];
+        }
+        else
+            j = nextVal[j];
+    }
+}
