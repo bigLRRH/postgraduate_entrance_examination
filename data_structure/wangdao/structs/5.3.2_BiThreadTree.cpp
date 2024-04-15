8918e2b57e1ca5965198ee5e1b4a3997ad12a70b
skyholeBishop <51252247+bigLRRH@users.noreply.github.com>
2024-04-15 20:28:12 +0800

    4/14

--- data_structure/wangdao/structs/5.3.2_BiThreadTree.cpp
+++ data_structure/wangdao/structs/5.3.2_BiThreadTree.cpp
@@ -1,11 +1,64 @@
 typedef struct BiThreadNode
 {
     int data;
-    BiThreadNode *lchild, *rchild;
+    BiThreadNode *leftChild, *rightChild;
     int leftTag, rightTag;
 } BiThreadNode, *BiThreadTree;
 
-void inOrderTraverse_thread()
+// create an inOrder binary thread tree
+void inOrderThreading(BiThreadTree currNode, BiThreadTree &preNode)
 {
-   
+    if (currNode)
+    {
+        inOrderThreading(currNode->leftChild, preNode);
+        if (!currNode->leftChild)
+        {
+            currNode->leftTag = 1;
+            currNode->leftChild = preNode;
+        }
+        if (preNode && !preNode->rightChild)
+        {
+            preNode->rightTag = 1;
+            preNode->rightChild = currNode;
+        }
+        preNode = currNode;
+        inOrderThreading(currNode->rightChild, preNode);
+    }
+}
+
+void createInOrderThread(BiThreadTree t)
+{
+    BiThreadTree preNode = nullptr;
+    if (t != nullptr)
+    {
+        inOrderThreading(t, preNode);
+        preNode->rightChild = nullptr;
+        preNode->rightTag = 1;
+    }
+}
+
+// traverse an inOrder binary thread tree
+void visit(BiThreadNode *node);
+BiThreadNode *firstNode(BiThreadNode *currNode)
+{
+    while (currNode->leftTag == 0)
+    {
+        currNode = currNode->leftChild;
+    }
+    return currNode;
+}
+BiThreadNode *nextNode(BiThreadNode *currNode)
+{
+    if (currNode->rightTag == 0)
+    {
+        return firstNode(currNode->rightChild);
+    }
+    return currNode->rightChild;
+}
+void inOrder(BiThreadTree t)
+{
+    for (BiThreadNode *currNode = t; currNode != nullptr; currNode = nextNode(currNode))
+    {
+        visit(currNode);
+    }
 }
